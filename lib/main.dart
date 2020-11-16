65e67ced520375737a614a48d2f8aaae69c09ed6
Kathryn El Shamy <k.elshamy@live.com>
2020-11-15 23:09:52 -0500

    Added 'home' dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() {
   runApp(MaterialApp(home: HomePage()));
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      child: Text(
-        'Buy Stuff',
-        style: TextStyle(color: Colors.pink),
-      ),
-    );
-  }
 }
