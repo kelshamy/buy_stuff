9eaa476796253829c04d90ed10057254673d50fa
Kathryn El Shamy <k.elshamy@live.com>
2020-11-18 18:17:19 -0500

    Created HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'home.dart';
 
 void main() {
   runApp(MaterialApp(home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Buy Stuff'),
+      ),
+    );
+  }
 }
